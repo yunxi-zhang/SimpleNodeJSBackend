02278d9a278fa1f52f2127ba52f0acedf1b5d4e6
yunxi.zhang <yunxi.zhang@hotmail.com>
2021-10-25 11:13:28 +0100

    update shell file to run 0.2

--- useLocalDockerImage.sh
+++ useLocalDockerImage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker rmi simplenodejsbackend:0.1
-docker build -t simplenodejsbackend:0.1 .
-docker run -dt -p 3000:3000 --name simplebackend simplenodejsbackend:0.1
+docker rmi simplenodejsbackend:0.2
+docker build -t simplenodejsbackend:0.2 .
+docker run -dt -p 9000:9000 --name simplebackend simplenodejsbackend:0.2
