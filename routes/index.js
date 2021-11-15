f56838f530ea6f07a21136f48affa3ccc2aff20b
yunxi.zhang <yunxi.zhang@hotmail.com>
2021-11-15 13:56:59 +0000

    add one more example

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,18 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/axios/promise', function (req, res, next) {
+router.get('/axios/promise/v1', function (req, res, next) {
   let config = {
     method: 'get',
     url: 'http://petstore-demo-endpoint.execute-api.com/petstore/pets'
   };
   axios(config)
+    .then(response => res.json(response.data))
+    .catch(error => res.json(error));
+});
+
+router.get('/axios/promise/v2', function (req, res, next) {
+  axios.get('http://petstore-demo-endpoint.execute-api.com/petstore/pets')
     .then(response => res.json(response.data))
     .catch(error => res.json(error));
 });
