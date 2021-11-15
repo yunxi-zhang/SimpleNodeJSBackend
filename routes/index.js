4c4a5b4139ff2f452a7c699c75f27c97382d3461
yunxi.zhang <yunxi.zhang@hotmail.com>
2021-11-15 13:53:41 +0000

    add example

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,31 @@ const axios = require('axios');
 const qs = require('qs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/axios', async function(req, res, next) {
+router.get('/axios/promise', function (req, res, next) {
   let config = {
-      method: 'get',
-      url: 'http://petstore-demo-endpoint.execute-api.com/petstore/pets'
+    method: 'get',
+    url: 'http://petstore-demo-endpoint.execute-api.com/petstore/pets'
   };
   axios(config)
     .then(response => res.json(response.data))
     .catch(error => res.json(error));
+});
+
+router.get('/axios/async', async function (req, res, next) {
+  let config = {
+    method: 'get',
+    url: 'http://petstore-demo-endpoint.execute-api.com/petstore/pets'
+  };
+  try {
+    let response = await axios(config);
+    return res.json(response.data);
+  } catch (error) {
+    return res.json(error);
+  }
 });
 
 module.exports = router;
