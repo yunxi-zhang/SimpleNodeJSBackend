ec7d09a9d4e8f008b9e0acb3c6d699f3adb1b59b
yunxi.zhang <yunxi.zhang@hotmail.com>
2021-11-15 12:41:44 +0000

    add axios example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+const qs = require('qs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/axios', async function(req, res, next) {
+  let config = {
+      method: 'get',
+      url: 'http://petstore-demo-endpoint.execute-api.com/petstore/pets'
+  };
+  axios(config)
+    .then(response => res.json(response.data))
+    .catch(error => res.json(error));
 });
 
 module.exports = router;
