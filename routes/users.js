3e2d5cfa90f8ac22362ef27051b656c6738cf306
yunxi.zhang <yunxi.zhang@hotmail.com>
2022-06-22 14:19:19 +0100

    add bearer token based api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/login', (req, res) => {
+  const payload = {
+    name: "yunxi"
+  }
+
+  const token = jwt.sign(payload, "secret", (err, token) => {
+    res.json({
+      token
+    })
+  })
+})
+
+router.get('/profile',verifyToken ,(req, res) => {
+
+  jwt.verify(req.token, "secret", (err, authData) => {
+    if(err) {
+      console.log("triggered 1");
+      res.status(401).send("Unauthorized");
+    }else {
+      res.json({
+        message: "Pass authentication"
+      })
+    }
+  })
+})
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    console.log('bearer:', bearer);
+    const bearerToken = bearer [1];
+    console.log('bearerToken:', bearerToken);
+    req.token = bearerToken;
+    next();
+  } else {
+    console.log("triggered 2");
+    res.status(401).send("Unauthorized");
+  }
+}
+
 
 module.exports = router;
